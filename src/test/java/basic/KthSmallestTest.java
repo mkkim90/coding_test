24cc6771f17c7ce5c1d23b83b28bf947627a1656
mkkim90 <mkzzang0928@gmail.com>
2021-03-28 00:44:57 +0900

    feat : kthSmallest

---
+++ src/test/java/basic/KthSmallestTest.java
@@ -0,0 +1,26 @@
+package basic;
+
+import apple.laf.JRSUIUtils;
+
+public class KthSmallestTest {
+    int k;
+    int i;
+    int ret;
+
+    private int kthSmallest(TreeNode root, int k) {
+        i = 0;
+        trace(root);
+        return ret;
+    }
+
+    private boolean trace(TreeNode root) {
+        if (root == null) return false;
+        boolean b = trace(root.getLeft());
+        if (b) return b;
+        i++;
+        if (i == k) {
+            ret = root.getVal();
+        }
+        return trace(root.getRight());
+    }
+}
