60d76c50e957cc349c5656905e34d43a36950564
mkkim90 <mkzzang0928@gmail.com>
2021-04-08 17:45:32 +0900

    feat : finally 호출 순서

--- src/test/java/codingInterview/chapt13/Problem2_finallyTest.java
+++ src/test/java/codingInterview/chapt13/Problem2_finallyTest.java
@@ -1,2 +1,36 @@
-package codingInterview.chapt13;public class Problem2_finallyTest {
+package codingInterview.chapt13;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Problem2_finallyTest {
+
+    @DisplayName("finally 호출 사이클")
+    @Test
+    void call() {
+        bar();
+    }
+
+    private void bar() {
+        System.out.println("start bar");
+        String v = foo();
+        System.out.println(v);
+        System.out.println("end bar");
+    }
+
+    private String foo() {
+        int x = 0;
+        int y = 5;
+        try {
+            System.out.println("start try");
+            int b = x / y;
+            System.out.println("end try");
+            return "return from try";
+        } catch (Exception ex) {
+            System.out.println("catch");
+            return "return from catch";
+        } finally {
+            System.out.println("finally");
+        }
+    }
 }
