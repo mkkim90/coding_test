4f1ef2d9baddb00164a61997061e79159df054a8
mkkim90 <mkzzang0928@gmail.com>
2021-04-11 14:11:35 +0900

    feat : 큰수 찾기 퀵정렬 활용

--- src/test/java/leetcode/KthLargestElementInAnArrayTest.java
+++ src/test/java/leetcode/KthLargestElementInAnArrayTest.java
@@ -30,4 +30,51 @@ public class KthLargestElementInAnArrayTest {
 
         return pq.peek();
     }
+    @Test
+    void solution2() {
+        int[] nums = {3,2,1,5,6,4};
+        assertThat(findKthLargestByQuick(nums, 2)).isEqualTo(5);
+    }
+
+
+    private int findKthLargestByQuick(int[] nums, int k) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums[nums.length - k];
+    }
+
+    void quickSort(int[] arr, int left, int right) {
+        int index = partition(arr, left, right);
+        if (left < index -1) {
+            quickSort(arr, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(arr, index, right);
+        }
+    }
+
+    int partition(int[] arr, int left, int right) {
+        int pivot = arr[(left + right) / 2]; // 분할 기준 원소 선정
+        while (left <= right) {
+            // 왼쪽에서 오른쪽으로 옮겨야 하는 원소 탐색
+            while (arr[left] < pivot) left++;
+
+            // 오른쪽에서 왼쪽으로 옮겨야 하는 원소 탐색
+            while (arr[right] > pivot) right--;
+
+            // 원소를 스왑한 뒤 left와 right를 이동
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
+    }
+
+    private void swap(int[] array, int a, int b) {
+        int temp = array[b];
+        array[b] = array[a];
+        array[a] = temp;
+    }
 }
