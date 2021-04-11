af8a2d98811caa16005dbc53659cd2e2c2c6cf07
mkkim90 <mkzzang0928@gmail.com>
2021-04-11 22:44:18 +0900

    fixed : Kth Smallest 디버깅을 위한 테스트 코드 구현 및 Longest Increasing Subsequence 동적 프로그래밍 재구현

--- src/test/java/leetcode/KthSmallestTest.java
+++ src/test/java/leetcode/KthSmallestTest.java
@@ -1,11 +1,35 @@
 package leetcode;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class KthSmallestTest {
     int k;
     int i;
     int ret;
 
-    private int kthSmallest(TreeNode root, int k) {
+    @Test
+    void solution() {
+
+        TreeNode one = new TreeNode(1);
+        TreeNode three = new TreeNode(3);
+        TreeNode two = new TreeNode(2);
+        TreeNode seven = new TreeNode(7);
+        TreeNode four = new TreeNode(4);
+        TreeNode five = new TreeNode(5);
+
+        two.setLeft(one);
+        three.setLeft(two);
+        three.setRight(four);
+        five.setLeft(three);
+        five.setRight(seven);
+        k=3;
+        assertThat(kthSmallest(five)).isEqualTo(3);
+
+    }
+
+    private int kthSmallest(TreeNode root) {
         i = 0;
         trace(root);
         return ret;
--- src/test/java/leetcode/LongestIncreasingSubsequenceTest.java
+++ src/test/java/leetcode/LongestIncreasingSubsequenceTest.java
@@ -8,7 +8,7 @@ public class LongestIncreasingSubsequenceTest {
     @Test
     void solution() {
         int[] nums = {10, 9, 2, 5, 3, 7, 101, 10};
-        assertThat(longestIncreasingSubsequence(nums)).isEqualTo(4);
+        assertThat(longestIncreasingSubsequenceWithDP(nums)).isEqualTo(4);
     }
 
     int longestIncreasingSubsequence(int[] nums) {
@@ -31,6 +31,21 @@ public class LongestIncreasingSubsequenceTest {
             if (d[i] > max) max = d[i];
         }
 
+        return max;
+    }
+
+    int longestIncreasingSubsequenceWithDP(int[] nums) {
+        int[] dp = new int[nums.length];
+        int max = 1;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(dp[i], max);
+                }
+            }
+        }
         return max;
     }
 
