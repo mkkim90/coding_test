317d54ac3dd97b0aa5e0d3ceeee9abb080c4762b
mkkim90 <mkzzang0928@gmail.com>
2021-04-16 16:41:13 +0900

    feat : 124 나라

--- src/test/java/programmers/OneTwoFourWorldTest.java
+++ src/test/java/programmers/OneTwoFourWorldTest.java
@@ -1,2 +1,24 @@
-package programmers;public class OneTwoFourWorldTest {
+package programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OneTwoFourWorldTest {
+
+    @Test
+    void solution() {
+        int num = 5;
+        assertThat(getOneTwoFour(num)).isEqualTo("12");
+    }
+
+    private String getOneTwoFour(int n) {
+        String[] strs = {"4", "1", "2"};
+        String answer = "";
+        while (n > 0) {
+            answer = strs[n % 3] + answer;
+            n = (n - 1) / 3;
+        }
+        return answer;
+    }
 }
