6731b54b8cc63dbfedb99098d847ff6a05dde39b
mkkim90 <mkzzang0928@gmail.com>
2021-04-17 13:34:02 +0900

    feat : 삼각달팽이

--- src/test/java/programmers/TriangularSnailTest.java
+++ src/test/java/programmers/TriangularSnailTest.java
@@ -1,2 +1,42 @@
-package programmers;public class TriangularSnailTest {
+package programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TriangularSnailTest {
+    @Test
+    void solution() {
+        assertThat(triagularSnail(4)).containsExactly(1,2,9,3,10,8,4,5,6,7);
+    }
+
+    private int[] triagularSnail(int n) {
+        int max = (n * (n + 1)) / 2;
+        int[][] metrix = new int[n][n];
+        int i = 0, j = 0, k = 1;
+        metrix[i][j] = k;
+
+        while (k < max) {
+            while (i + 1 < n && metrix[i+1][j] == 0) {
+                metrix[++i][j] = ++k;
+            }
+
+            while (j + 1 < n && metrix[i][j+1] == 0) {
+                metrix[i][++j] = ++k;
+            }
+
+            while (j - 1 > 0 && i - 1 > 0 && metrix[i-1][j-1] == 0) {
+                metrix[--i][--j] = ++k;
+            }
+        }
+
+        int[] result = new int[max];
+        int index = 0;
+        for (int l = 0; l < n; l++) {
+            for (int m = 0; m <= l; m++) {
+                result[index++] = metrix[l][m];
+            }
+        }
+        return result;
+    }
 }
