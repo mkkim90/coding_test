122f97905bb4cd320304eb1f85976d4244159768
mkkim90 <mkzzang0928@gmail.com>
2021-04-22 15:47:29 +0900

    feat : 구명보트

---
+++ src/test/java/programmers/LifeBoatTest.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * [70, 50, 80, 50]	100	3
+ * [70, 80, 50]	100	3
+ */
+public class LifeBoatTest {
+
+    @Test
+    void solution() {
+        int[] people = {70, 80, 50};
+        int limit = 100;
+        assertThat(countLifeBoat(people, limit)).isEqualTo(3);
+    }
+
+    private int countLifeBoat(int[] people, int limit) {
+        int answer = 0;
+        int i = 0;
+        Arrays.sort(people);
+        for(int j=people.length-1; i<=j; j--) {
+            answer++;
+            if(people[j] + people[i] <= limit) {
+                i++;
+            }
+        }
+        return answer;
+    }
+}
