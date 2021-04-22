7221208b7b00fc055fa222a7a2cb95c174c5162b
mkkim90 <mkzzang0928@gmail.com>
2021-04-22 14:51:41 +0900

    refactor : 프로그래머스 괄호변환

--- src/test/java/programmers/parenthesisTranslationTest.java
+++ src/test/java/programmers/parenthesisTranslationTest.java
@@ -2,8 +2,6 @@ package programmers;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Stack;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -23,6 +21,43 @@ public class parenthesisTranslationTest {
         assertThat(getCorrectParenthesis("()))((()")).isEqualTo("()(())()");
     }
 
+
+    private String getCorrectParenthesis(String balancedParenthesis) {
+        // 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
+        if (balancedParenthesis.length() == 0) return balancedParenthesis;
+
+        // 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상
+        //	 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
+        int open = 0;
+        int close = 0;
+        for (char c : balancedParenthesis.toCharArray()) {
+            if (c == '(') open++;
+            else close++;
+            if (open == close) break;
+        }
+        int index = open + close;
+        String u = balancedParenthesis.substring(0, index);
+        String v = balancedParenthesis.substring(index);
+        // 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
+        if (isCorrectParenthesis(u)) {
+            return u + getCorrectParenthesis(v);
+        }
+        // 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
+        StringBuffer sb = new StringBuffer();
+        sb.append("(");
+        sb.append(getCorrectParenthesis(v));
+        sb.append(")");
+        for (int i = 1; i < uLen - 1; i++) {
+            if (u.charAt(i) == '(') {
+                sb.append(")");
+            } else {
+                sb.append("(");
+            }
+        }
+        return sb.toString();
+
+    }
+
     private boolean isCorrectParenthesis(String balancedParenthesis) {
         int strLen = balancedParenthesis.length();
         int open = 0;
@@ -35,44 +70,5 @@ public class parenthesisTranslationTest {
             }
         }
         return true;
-    }
-
-    private String getCorrectParenthesis(String balancedParenthesis) {
-        // 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
-        if (balancedParenthesis.length() == 0) return balancedParenthesis;
-
-        // 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
-        int open = 0;
-        int close = 0;
-        for (char c : balancedParenthesis.toCharArray()) {
-            if (c == '(') open++;
-            else close++;
-            if (open == close) break;
-        }
-        int uLen = open + close;
-        String u = balancedParenthesis.substring(0, uLen);
-        String v = balancedParenthesis.substring(uLen);
-
-        if (isCorrectParenthesis(u)) {
-            // 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
-            //   3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
-            return u + getCorrectParenthesis(v);
-        } else {
-            // 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
-            //   4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
-            //   4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
-            //   4-3. ')'를 다시 붙입니다.
-            String uDash = "(" + getCorrectParenthesis(v) + ")";
-            //   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
-            for (int i = 1; i < uLen - 1; i++) {
-                if (u.charAt(i) == '(') {
-                    uDash += ")";
-                } else {
-                    uDash += "(";
-                }
-            }
-            //   4-5. 생성된 문자열을 반환합니다.
-            return uDash;
-        }
     }
 }
