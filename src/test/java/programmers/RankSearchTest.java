afbaf047cf9774b183e5a975f381612918d56166
mkkim90 <mkzzang0928@gmail.com>
2021-04-25 01:38:20 +0900

    refactor : 성능 효율 이슈

--- src/test/java/programmers/RankSearchTest.java
+++ src/test/java/programmers/RankSearchTest.java
@@ -3,78 +3,96 @@ package programmers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RankSearchTest {
-    private Applicants applicants;
+    private Map<String, List<Integer>> map = new HashMap<>();
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void rankSearchTest() {
         String[] infos = {"java backend junior pizza 150",
                 "python frontend senior chicken 210",
                 "python frontend senior chicken 150",
                 "cpp backend senior pizza 260",
                 "java backend junior chicken 80",
                 "python backend senior chicken 50"};
-        applicants = new Applicants(infos);
+        String[] querys = {"java and backend and junior and pizza 100",
+                "python and frontend and senior and chicken 200",
+                "cpp and - and senior and pizza 250",
+                "- and backend and senior and - 150",
+                "- and - and - and chicken 100",
+                "- and - and - and - 150"};
+        assertThat(solution(infos, querys)).containsExactly(1,1,1,1,2,4);
     }
 
-    @Test
-    void solution() {
-        Applicant applicant = new Applicant("java backend junior pizza 150");
-        assertThat(applicant.getLanguage()).isEqualTo("java");
-        assertThat(applicant.getJobGroup()).isEqualTo("backend");
-        assertThat(applicant.getCareer()).isEqualTo("junior");
-        assertThat(applicant.getSoulFood()).isEqualTo("pizza");
-        assertThat(applicant.getScore()).isEqualTo(150);
+    private void combination(String[] arr, boolean[] visited, int start, int r) {
+        if (r == 0) {
+            StringBuffer query = new StringBuffer();
+            for (int i = 0; i < 4; i++) {
+                if(visited[i]) query.append(arr[i]);
+            }
+
+            map.computeIfAbsent(query.toString(), s -> new ArrayList<>()).add(Integer.parseInt(arr[4]));
+            return;
+        }
+
+        for (int i = start; i < 4; i++) {
+            visited[i] = true;
+            combination(arr, visited, i + 1, r - 1);
+            visited[i] = false;
+        }
     }
 
-    @Test
-    void applicantsOfTest() {
-        String[] infos = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
-        List<Applicant> list = Applicants.of(infos);
-        assertThat(Applicants.of(infos).size()).isEqualTo(6);
-        assertThat(list.get(0).getCareer()).isEqualTo("junior");
+    public int[] solution(String[] infos, String[] query) {
+        combinationProcess(infos);
+        sort();
+        return rankSearchCounts(query);
     }
 
-    @Test
-    void isSameLanguageTest() {
-        assertThat(applicants.filterSameLanguages("java").size()).isEqualTo(2);
-        assertThat(applicants.filterSameLanguages("python").size()).isEqualTo(3);
+    private void sort() {
+        for (Map.Entry<String, List<Integer>> entry : map.entrySet()){
+            entry.getValue().sort(null);
+        }
     }
 
-    @Test
-    void isSameJobGroupTest() {
-        assertThat(applicants.filterSameJobGroups("backend").size()).isEqualTo(4);
-        assertThat(applicants.filterSameJobGroups("frontend").size()).isEqualTo(2);
+    private int[] rankSearchCounts(String[] query) {
+        int[] answer = new int[query.length];
+        for(int i = 0; i < query.length; i++) {
+            String[] splits = query[i].replaceAll("-", "").replaceAll(" and ", "").split(" ");
+            String key = splits[0];
+            int score = Integer.parseInt(splits[1]);
+            List<Integer> list = map.getOrDefault(key, new ArrayList<>());
+            int matchCount = binarySearch(score, list);
+            answer[i] = matchCount;
+        }
+        return answer;
     }
 
-    @Test
-    void filterSameCareersTest() {
-        assertThat(applicants.filterSameCareers("junior").size()).isEqualTo(2);
-        assertThat(applicants.filterSameCareers("senior").size()).isEqualTo(4);
+    private void combinationProcess(String[] infos) {
+        for (String info : infos) {
+            String[] splits = info.split(" ");
+            boolean[] visited = new boolean[splits.length];
+
+            for (int i = 0; i < splits.length; i++) {
+                combination(splits, visited, 0, i);
+            }
+        }
     }
 
-    @Test
-    void filterSameSoulFoodTest() {
-        assertThat(applicants.filterSameSoulFood("pizza").size()).isEqualTo(2);
-        assertThat(applicants.filterSameSoulFood("chicken").size()).isEqualTo(4);
-    }
-
-    @Test
-    void moreThanScoreTest() {
-        assertThat(applicants.filterMoreThanScore("150").size()).isEqualTo(4);
-    }
-
-    @Test
-    void filterCountTest() {
-        assertThat(applicants.filterCount("java and backend and junior and pizza 100")).isEqualTo(1);
-        assertThat(applicants.filterCount("python and frontend and senior and chicken 200")).isEqualTo(1);
-        assertThat(applicants.filterCount("cpp and - and senior and pizza 250")).isEqualTo(1);
-        assertThat(applicants.filterCount("- and backend and senior and - 150")).isEqualTo(1);
-        assertThat(applicants.filterCount("- and - and - and chicken 100")).isEqualTo(2);
-        assertThat(applicants.filterCount("- and - and - and - 150")).isEqualTo(4);
+    private int binarySearch(int score, List<Integer> list) {
+        int start = 0;
+        int end = list.size();
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (list.get(mid) < score) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        int matchCount = list.size() - start;
+        return matchCount;
     }
 }
