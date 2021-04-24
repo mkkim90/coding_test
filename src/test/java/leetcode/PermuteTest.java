9df8275f39fb4709c8a9a95bf622a01b816e7721
mkkim90 <mkzzang0928@gmail.com>
2021-04-24 13:54:00 +0900

    modify : dfs 추가

--- src/test/java/leetcode/PermuteTest.java
+++ src/test/java/leetcode/PermuteTest.java
@@ -40,4 +40,66 @@ public class PermuteTest {
             tmp.remove(tmp.size() - 1);
         }
     }
+
+    @Test
+    void dfs() {
+        int[] nums = {1, 2, 3};
+        int[] output = new int[nums.length];
+        boolean[] visited = new boolean[nums.length];
+
+        dfs(nums, output, visited, 0, nums.length, 3);
+    }
+
+    private void dfs(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
+        if (depth == r) {
+            print(output, r);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] != true) {
+                visited[i] = true;
+                output[depth] = arr[i];
+                dfs(arr, output, visited, depth + 1, n, r);
+                output[depth] = 0; // 이 줄은 없어도 됨
+                visited[i] = false;
+                ;
+            }
+        }
+    }
+
+    private void print(int[] output, int r) {
+        for (int i = 0; i < r; i++) {
+            System.out.println(output[i]);
+        }
+        System.out.println("");
+    }
+
+    @Test
+    private void dfsSwapTest() {
+        int[] nums = {1, 2, 3};
+        int[] output = new int[nums.length];
+        boolean[] visited = new boolean[nums.length];
+
+        dfsSwap(nums,0, nums.length, 3);
+    }
+
+    void dfsSwap(int[] arr, int depth, int n, int r) {
+        if (depth == r) {
+            print(arr, r);
+            return;
+        }
+
+        for (int i = depth; i < n; i++) {
+            swap(arr, depth, i);
+            dfsSwap(arr, depth + 1, n, r);
+            swap(arr, depth, i);
+        }
+    }
+
+    private void swap(int[] arr, int depth, int i) {
+        int temp = arr[depth];
+        arr[depth] = arr[i];
+        arr[i] = temp;
+    }
 }
