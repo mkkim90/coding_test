1cedcc978b45603cb6782aa8ba2579ac4420de58
mkkim90 <mkzzang0928@gmail.com>
2021-04-27 16:32:12 +0900

    test : 큐 테스트

--- src/test/java/codingInterview/chapt3/MyQueueTest.java
+++ src/test/java/codingInterview/chapt3/MyQueueTest.java
@@ -1,2 +1,27 @@
-package codingInterview.chapt3;public class MyQueueTest {
+package codingInterview.chapt3;
+
+import chapt3.MyQueue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MyQueueTest {
+    private MyQueue queue;
+
+    @BeforeEach
+    void setQueue() {
+        queue = new MyQueue<Integer>();
+    }
+
+    @Test
+    void test() {
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        assertThat(queue.remove()).isEqualTo(1);
+        assertThat(queue.remove()).isEqualTo(2);
+        assertThat(queue.remove()).isEqualTo(3);
+        assertThat(queue.isEmpty()).isTrue();
+    }
 }
