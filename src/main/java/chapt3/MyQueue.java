25f96f85a69dbdcaa2e7931bedd2a9e8af112025
mkkim90 <mkzzang0928@gmail.com>
2021-04-27 16:31:52 +0900

    feat : 큐 구현

--- src/main/java/chapt3/MyQueue.java
+++ src/main/java/chapt3/MyQueue.java
@@ -1,2 +1,48 @@
-package chapt3;public class MyQueue {
+package chapt3;
+
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> {
+    private static class QueueNode<T> {
+        private T data;
+        private QueueNode next;
+        public QueueNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+    public void add(T item) {
+        QueueNode t = new QueueNode(item);
+        if (last != null)
+            last.next = t;
+
+        last = t;
+
+        if (first == null)
+            first = last;
+    }
+
+    public T remove() {
+        if (first == null ) throw new NoSuchElementException();
+
+        T item = first.data;
+        first = first.next;
+        if (first == null) {
+            last = null;
+        }
+
+        return item;
+    }
+
+    public T peek() {
+        if (first == null) throw new NoSuchElementException();
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
 }
