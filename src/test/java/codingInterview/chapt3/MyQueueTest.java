82f5ef412eeeba8039ea8ed886182347b607b48b
mkkim90 <mkzzang0928@gmail.com>
2021-04-29 14:26:14 +0900

    test : 구현 큐 테스트

--- src/test/java/codingInterview/chapt3/MyQueueTest.java
+++ src/test/java/codingInterview/chapt3/MyQueueTest.java
@@ -4,6 +4,9 @@ import chapt3.MyQueue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyQueueTest {
@@ -23,5 +26,16 @@ public class MyQueueTest {
         assertThat(queue.remove()).isEqualTo(2);
         assertThat(queue.remove()).isEqualTo(3);
         assertThat(queue.isEmpty()).isTrue();
+    }
+
+    @Test
+    void real() {
+        Queue queue = new LinkedList<Integer>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        assertThat(queue.remove()).isEqualTo(1);
+        assertThat(queue.remove()).isEqualTo(2);
+        assertThat(queue.remove()).isEqualTo(3);
     }
 }
