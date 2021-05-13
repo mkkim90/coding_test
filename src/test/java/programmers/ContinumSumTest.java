a9e2f104973c2f0b035220620d3b411ea40eb55c
mkkim90 <mkzzang0928@gmail.com>
2021-05-13 17:56:19 +0900

    feat : 프로그래머스 숫자표현

--- src/test/java/programmers/ContinumSumTest.java
+++ src/test/java/programmers/ContinumSumTest.java
@@ -1,2 +1,29 @@
-package programmers;public class ContinumSumTest {
+package programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ContinumSumTest {
+    @Test
+    void solution() {
+        assertThat(continueSum(15)).isEqualTo(4);
+    }
+
+    private int continueSum(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n ; i++) {
+            int sum = 0;
+            for (int k = i; k <= n; k++) {
+                sum += k;
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
 }
