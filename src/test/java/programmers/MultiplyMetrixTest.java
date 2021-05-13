81249dd4259140a9f40ba9aa40b0a3251f52eb0d
mkkim90 <mkzzang0928@gmail.com>
2021-05-13 17:57:04 +0900

    feat : 프로그래머스 행렬곱셈

--- src/test/java/programmers/MultiplyMetrixTest.java
+++ src/test/java/programmers/MultiplyMetrixTest.java
@@ -1,2 +1,20 @@
-package programmers;public class MultiplyMetrixTest {
+package programmers;
+
+/**
+ * C[i][j] = A[i][1]B[1][j] + A[i][2]B[2][j] + ... + A[i][n]*B[n][j]
+ */
+public class MultiplyMetrixTest {
+
+    private int[][] multiplyMetrix(int[][] a, int[][] b) {
+        int[][] answer = new int[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                int sum = 0;
+                for (int k = 0; k < b.length; k++) {
+                    answer[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return answer;
+    }
 }
