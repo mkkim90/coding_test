2648db0b8908ed8e3c10a76ec52820a141998c86
mkkim90 <mkzzang0928@gmail.com>
2021-05-22 15:48:14 +0900

    feat : 릿코드 LongestSubstringWithoutRepeatingCharacters

--- src/test/java/leetcode/LongestSubstringWithoutRepeatingCharactersTest.java
+++ src/test/java/leetcode/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -1,2 +1,33 @@
-package leetcode;public class LongestSubstringWithoutRepeatingCharactersTest {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LongestSubstringWithoutRepeatingCharactersTest {
+    @Test
+    void solution() {
+        String s = "pwwkew";
+        assertThat(lengthOfLongestSubstring(s)).isEqualTo(1);
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int i = 0, j = 0, max = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+
+        return max;
+    }
+
 }
