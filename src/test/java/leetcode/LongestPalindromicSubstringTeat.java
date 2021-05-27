96168380bb1998b981350843808e8fb413386ed9
mkkim90 <mkzzang0928@gmail.com>
2021-05-27 21:31:18 +0900

    feat 릿코드

--- src/test/java/leetcode/LongestPalindromicSubstringTeat.java
+++ src/test/java/leetcode/LongestPalindromicSubstringTeat.java
@@ -1,2 +1,52 @@
-package leetcode;public class LongestPalindromicSubstringTeat {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Example 1:
+ *
+ * Input: s = "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example 2:
+ *
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * Example 3:
+ *
+ * Input: s = "a"
+ * Output: "a"
+ * Example 4:
+ *
+ * Input: s = "ac"
+ * Output: "a"
+ *
+ */
+public class LongestPalindromicSubstringTeat {
+
+    @Test
+    void solution() {
+        assertThat(longestPalindrome("babad")).isEqualTo("bab");
+    }
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        String res = null;
+
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+                if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+                    res = s.substring(i, j + 1);
+                }
+            }
+        }
+
+        return res;
+    }
 }
