cf73e7ee8f2fb96881fe70e842e2aa55e2b0147b
mkkim90 <mkzzang0928@gmail.com>
2021-05-28 22:48:40 +0900

    feat : leetcode maxarea

--- src/test/java/leetcode/MaxAreaTest.java
+++ src/test/java/leetcode/MaxAreaTest.java
@@ -1,2 +1,25 @@
-package leetcode;public class MaxAreaTest {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MaxAreaTest {
+    @Test
+    void solution() {
+        int[] nums = {1,8,6,2,5,4,8,3,7};
+        assertThat(maxArea(nums)).isEqualTo(49);
+    }
+
+    private int maxArea(int[] nums) {
+        int max = 0;
+        int right = nums.length - 1;
+        int left = 0;
+        while (left < right) {
+            max = Math.max(max, Math.min(nums[right], nums[left])*(right-left));
+            if (nums[right] > nums[left]) left++;
+            else right--;
+        }
+        return max;
+    }
 }
