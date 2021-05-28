859ab07fcee552a691c5951042f064c2a6a78479
mkkim90 <mkzzang0928@gmail.com>
2021-05-28 22:49:02 +0900

    feat : leetcode merge tow sorted list

--- src/test/java/leetcode/MergeTwoSortedListsTest.java
+++ src/test/java/leetcode/MergeTwoSortedListsTest.java
@@ -1,2 +1,28 @@
-package leetcode;public class MergeTwoSortedListsTest {
+package leetcode;
+
+public class MergeTwoSortedListsTest {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode prev = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else if (l1.val > l2.val) {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        if (l1 != null) {
+            prev.next = l1;
+        }
+
+        if (l2 != null) {
+            prev.next = l2;
+        }
+
+        return result.next;
+    }
 }
