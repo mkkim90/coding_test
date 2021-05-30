0b54727a4925a1c835f7951cdbd0da18e597467f
mkkim90 <mkzzang0928@gmail.com>
2021-05-30 17:02:03 +0900

    feat : 회전

--- src/test/java/leetcode/RotateImageTest.java
+++ src/test/java/leetcode/RotateImageTest.java
@@ -1,2 +1,24 @@
-package leetcode;public class RotateImageTest {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class RotateImageTest {
+    @Test
+    void solution() {
+        //int[][] metrix = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        int[][] metrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(metrix);
+    }
+
+    public void rotate(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+    }
 }
