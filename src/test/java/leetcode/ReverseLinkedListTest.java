959b00f0ad7afb0d28aa39261187e5e0764d7638
mkkim90 <mkzzang0928@gmail.com>
2021-06-01 17:17:20 +0900

    feat : 릿코드 reverse linked list

--- src/test/java/leetcode/ReverseLinkedListTest.java
+++ src/test/java/leetcode/ReverseLinkedListTest.java
@@ -1,2 +1,37 @@
-package leetcode;public class ReverseLinkedListTest {
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ReverseLinkedListTest {
+    private ListNode head;
+
+    @BeforeEach
+    void setUp() {
+        head = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
+        head.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = five;
+    }
+
+    @Test
+    void solution() {
+        reverseList(head);
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
 }
