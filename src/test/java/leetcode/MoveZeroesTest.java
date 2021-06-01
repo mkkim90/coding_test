aaa7d01faff168ba556d721277f33f652ed22957
mkkim90 <mkzzang0928@gmail.com>
2021-06-01 17:16:45 +0900

    feat : 릿코드 move zeroes

--- src/test/java/leetcode/MoveZeroesTest.java
+++ src/test/java/leetcode/MoveZeroesTest.java
@@ -1,2 +1,25 @@
-package leetcode;public class MoveZeroesTest {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoveZeroesTest {
+    @Test
+    void solution() {
+        int[] nums = {0,1,0,3,12};
+        assertThat(moveZeroes(nums)).containsExactly(1,3,12,0,0);
+    }
+
+    public int[] moveZeroes(int[] nums) {
+        int i = 0;
+        for (int num : nums) {
+            if (num != 0) nums[i++] = num;
+        }
+
+        for (int j = i; j < nums.length; j++) {
+            nums[j] = 0;
+        }
+        return nums;
+    }
 }
