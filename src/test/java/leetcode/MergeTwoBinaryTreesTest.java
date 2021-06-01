ff99e5a4a2286990c9bdeb9135cb4cec55086de3
mkkim90 <mkzzang0928@gmail.com>
2021-06-01 17:09:11 +0900

    feat : 릿코드 merge two binary tree

--- src/test/java/leetcode/MergeTwoBinaryTreesTest.java
+++ src/test/java/leetcode/MergeTwoBinaryTreesTest.java
@@ -1,2 +1,24 @@
-package leetcode;public class MergeTwoBinaryTreesTest {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class MergeTwoBinaryTreesTest {
+
+    @Test
+    void solution() {
+
+    }
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return null;
+        int val = (root1 == null ? 0 : root1.getVal()) + (root2 == null ? 0 : root2.getVal());
+        TreeNode merge = new TreeNode(val);
+        TreeNode rigtTree = mergeTrees(root1.getRight(),root2.getRight());
+        merge.setRight(rigtTree);
+        TreeNode leftTree = mergeTrees(root1.getLeft(), root2.getLeft());
+        merge.setLeft(leftTree);
+        return merge;
+    }
+
+
 }
