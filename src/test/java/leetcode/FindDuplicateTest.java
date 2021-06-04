4b8b5061c79297154de5d7e16c2e028448102645
mkkim90 <mkzzang0928@gmail.com>
2021-06-04 13:24:55 +0900

    feat : leetcode find duplicate

--- src/test/java/leetcode/FindDuplicateTest.java
+++ src/test/java/leetcode/FindDuplicateTest.java
@@ -1,2 +1,29 @@
-package leetcode;public class FindDuplicateTest {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FindDuplicateTest {
+    @Test
+    void solution() {
+        int[] nums = {1,3,4,2,2};
+        assertThat(findDuplicate(nums)).isEqualTo(2);
+    }
+
+
+    private int findDuplicate(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[Math.abs(nums[i])];
+
+            if (num >= 0) {
+                nums[Math.abs(nums[i])] *= -1;
+            } else {
+                return Math.abs(nums[i]);
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
 }
