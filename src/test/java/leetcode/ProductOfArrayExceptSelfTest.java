e9b896538ec6b85574652f9dd1d0476226120062
mkkim90 <mkzzang0928@gmail.com>
2021-06-04 13:29:04 +0900

    feat : product of array except self

--- src/test/java/leetcode/ProductOfArrayExceptSelfTest.java
+++ src/test/java/leetcode/ProductOfArrayExceptSelfTest.java
@@ -1,2 +1,25 @@
-package leetcode;public class ProductOfArrayExceptSelfTest {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductOfArrayExceptSelfTest {
+    @Test
+    void solution() {
+        int[] nums = {1,2,3,4};
+        int[] result = new int[nums.length];
+        for (int i = 0, temp = 1; i < nums.length; i++) {
+            result[i] = temp;
+            temp *= nums[i];
+        }
+
+        for (int i = nums.length-1, temp = 1; i >= 0; i--) {
+            result[i] *= temp;
+            temp *= nums[i];
+        }
+
+        assertThat(result).containsExactly(24,12,8,6);
+    }
+
 }
