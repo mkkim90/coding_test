ebfd0c8aa9bdd58d8d407c46f9211ab9bf5a433e
mkkim90 <mkzzang0928@gmail.com>
2021-06-04 13:59:28 +0900

    feat: 세개 합

--- src/test/java/leetcode/ThreeSumTest.java
+++ src/test/java/leetcode/ThreeSumTest.java
@@ -1,2 +1,33 @@
-package leetcode;public class ThreeSumTest {
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class ThreeSumTest {
+    @Test
+    void solution() {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        threeSum(nums);
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+        if (nums.length == 0) return new ArrayList<>(res);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) res.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                else if (sum > 0) k--;
+                else j++;
+            }
+
+        }
+        return new ArrayList<>(res);
+    }
+
+
 }
