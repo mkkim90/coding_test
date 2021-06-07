7e59d84fba5234fc0006a4e90d47ea1e4e215e1e
mkkim90 <mkzzang0928@gmail.com>
2021-06-07 12:22:36 +0900

    feat : 주식 투자

--- src/test/java/backjoon/StockInvestmentTest.java
+++ src/test/java/backjoon/StockInvestmentTest.java
@@ -1,2 +1,33 @@
-package backjoon;public class StockInvestmentTest {
+package backjoon;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+/**
+ * 2
+ * 4
+ * 500 800 200
+ * 300 0 300
+ * -100 -200 -400
+ * 600 200 300
+ * 3
+ * 451 234 309
+ * 224 334 467
+ * 143 246 245
+ */
+public class StockInvestmentTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int nums = sc.nextInt();
+        for (int i = 0; i < nums; i++) {
+            final int days = sc.nextInt();
+            int totalProfit = 0;
+            for (int j = 0; j < days; j++) {
+                int maxProfit = Math.max(Math.max(sc.nextInt(), sc.nextInt()), sc.nextInt());
+                if (maxProfit > 0) totalProfit += maxProfit;
+            }
+            System.out.println(totalProfit);
+        }
+    }
 }
