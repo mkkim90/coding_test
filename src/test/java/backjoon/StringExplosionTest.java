3a8a04348002f1c981b692ca7a5752f5a584eca6
mkkim90 <mkzzang0928@gmail.com>
2021-06-07 12:22:55 +0900

    feat : 문자열 폭발

--- src/test/java/backjoon/StringExplosionTest.java
+++ src/test/java/backjoon/StringExplosionTest.java
@@ -1,2 +1,26 @@
-package backjoon;public class StringExplosionTest {
+package backjoon;
+
+import java.util.Scanner;
+
+public class StringExplosionTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        String explosion = scan.next();
+
+        String result = explose(s, explosion);
+        if (result.length() == 0) System.out.println("FRULA");
+        else System.out.println(result);
+
+
+    }
+
+    private static String explose(String s, String explosion) {
+        int idx = s.indexOf(explosion);
+        if (idx == -1) return s;
+        StringBuffer sb = new StringBuffer();
+        sb.append(s.substring(0, idx));
+        sb.append(s.substring(idx + explosion.length()));
+        return explose(sb.toString(), explosion);
+    }
 }
