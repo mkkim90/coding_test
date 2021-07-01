6d41fb58e20e9eb7e79c020e10b485a2425c9e16
mkkim90 <mkzzang0928@gmail.com>
2021-07-01 14:13:36 +0900

    feat : 백준 안전구역

---
+++ src/test/java/backjoon/SafetyZoneTest.java
@@ -0,0 +1,49 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class SafetyZoneTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int[][] zones = new int[length][length];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                zones[i][j] = sc.nextInt();
+                if (max < zones[i][j]) max = zones[i][j];
+            }
+        }
+
+        System.out.println(maxCount(zones, max));
+    }
+
+    private static int maxCount(int[][] zones, int max) {
+        int maxCount = 0;
+        for (int i = 0; i <= max; i++) {
+            maxCount = Math.max(maxCount, countSaftyZone(new boolean[zones.length][zones[0].length], zones, i));
+        }
+        return maxCount;
+    }
+
+    private static int countSaftyZone(boolean[][] visited, int[][] zones, int limit) {
+        int count = 0;
+        for (int i = 0; i < zones.length; i++) {
+            for (int j = 0; j < zones[0].length; j++) {
+                if (visited[i][j] || zones[i][j] <= limit) continue;
+                dfs(visited, zones, i, j, limit);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(boolean[][] visited, int[][] zones, int i, int j, int limit) {
+        if (i < 0 || i >= zones.length || j < 0 || j >= zones[0].length || visited[i][j] || zones[i][j] <= limit) return;
+        visited[i][j] = true;
+        dfs(visited, zones, i + 1, j, limit); // 상
+        dfs(visited, zones, i - 1, j, limit); // 하
+        dfs(visited, zones, i, j - 1, limit); // 좌
+        dfs(visited, zones, i, j + 1, limit); // 우
+    }
+}
