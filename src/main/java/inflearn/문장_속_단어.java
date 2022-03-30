473d624d3eee0f16da9e282342a4ab1b5065002d
mkkim90 <mkzzang0928@gmail.com>
2022-03-30 23:06:11 +0900

    feat : 문장속단어

--- src/main/java/inflearn/문장_속_단어.java
+++ src/main/java/inflearn/문장_속_단어.java
@@ -2,20 +2,36 @@ package inflearn;
 
 import java.util.Scanner;
 
-public class 대소문자_변환 {
+public class 문장_속_단어 {
     public String solution(String str) {
         String answer = "";
-        for (char x : str.toCharArray()) {
-            if (Character.isLowerCase(x)) answer += Character.toUpperCase(x);
-            else answer += Character.toLowerCase(x);
+        int m = Integer.MIN_VALUE, pos;
+        /*String[] s = str.split(" ");
+        for (String x : s) {
+            int len = x.length();
+            if (len > m) {
+                m = len;
+                answer = x;
+            }
+        }*/
+        /* substring */
+        while ((pos = str.indexOf(' ')) != -1) {
+            String tmp = str.substring(0, pos);
+            int len = tmp.length();
+            if (len > m) {
+                m = len;
+                answer = tmp;
+            }
+            str = str.substring(pos + 1);
         }
+        if (str.length() > m) answer = str;
         return answer;
     }
 
     public static void main(String[] args) {
-        대소문자_변환 t = new 대소문자_변환();
+        문장_속_단어 t = new 문장_속_단어();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
+        String str = kb.nextLine();
         System.out.println(t.solution(str));
     }
 }
