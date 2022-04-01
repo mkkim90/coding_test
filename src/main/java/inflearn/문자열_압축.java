4e9cc6cce3a4a4c3533c0186660f3cf151f6033e
mkkim90 <mkzzang0928@gmail.com>
2022-04-01 20:32:27 +0900

    feat : 문자열 압축

--- src/main/java/inflearn/문자열_압축.java
+++ src/main/java/inflearn/문자열_압축.java
@@ -2,22 +2,26 @@ package inflearn;
 
 import java.util.Scanner;
 
-public class 문자찾기 {
-    public int solution(String str, char t) {
-        int answer=0;
-        str = str.toUpperCase();
-        t = Character.toUpperCase(t);
-        for (char x : str.toCharArray()) {
-            if (x == t) answer++;
+public class 문자열_압축 {
+    public String solution(String s) {
+        String answer = "";
+        s = s + " ";
+        int cnt = 1;
+        for (int i = 0; i < s.length()-1; i++) {
+            if (s.charAt(i) == s.charAt(i+1)) cnt++;
+            else {
+                answer += s.charAt(i);
+                if (cnt > 1) answer += String.valueOf(cnt);
+                cnt = 1;
+            }
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        문자찾기 t = new 문자찾기();
+        문자열_압축 t = new 문자열_압축();
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
-        char c = kb.next().charAt(0);
-        System.out.println(t.solution(str, c));
+        System.out.println(t.solution(str));
     }
 }
