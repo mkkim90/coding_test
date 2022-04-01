7a4965deb6db70893dc30f8405995bbdcb9fe3ea
mkkim90 <mkzzang0928@gmail.com>
2022-04-01 12:56:51 +0900

    feat : 숫자만 추출

--- src/main/java/inflearn/숫자만_추출.java
+++ src/main/java/inflearn/숫자만_추출.java
@@ -2,19 +2,20 @@ package inflearn;
 
 import java.util.Scanner;
 
-public class 팰린드롬 {
-    public String solution(String s) {
+public class 숫자만_추출 {
+    public int solution(String s) {
         String answer = "";
-        s = s.toUpperCase().replaceAll("[^A-Z]]", "");
-        String tmp = new StringBuilder(s).reverse().toString();
-        if (s.equals(tmp)) answer = "YES";
-        return answer;
+        for (char x : s.toCharArray()) {
+            //if (x > = 48 && x <=57) answer = answer * 10 + (x-48);
+            if (Character.isDigit(x)) answer += x;
+        }
+        return Integer.parseInt(answer);
     }
 
     public static void main(String[] args) {
-        팰린드롬 t = new 팰린드롬();
+        숫자만_추출 t = new 숫자만_추출();
         Scanner kb = new Scanner(System.in);
-        String str = kb.nextLine();
+        String str = kb.next();
         System.out.println(t.solution(str));
     }
 }
