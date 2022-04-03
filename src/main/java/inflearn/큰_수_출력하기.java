69e9bcc14620c977304996285b9b5335ab80237e
mkkim90 <mkzzang0928@gmail.com>
2022-04-03 17:08:01 +0900

    feat : 큰 수 출력하기

--- src/main/java/inflearn/큰_수_출력하기.java
+++ src/main/java/inflearn/큰_수_출력하기.java
@@ -1,24 +1,29 @@
 package inflearn;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class 암호 {
-    public String solution(int n, String s) {
-        String answer = "";
-        for (int i = 0; i < n; i++) {
-            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
-            int num = Integer.parseInt(tmp, 2);
-            answer += (char) num;
-            s = s.substring(7);
+public class 큰_수_출력하기 {
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i-1]) answer.add(arr[i]);
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        암호 t = new 암호();
+        큰_수_출력하기 t = new 큰_수_출력하기();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        String str = kb.next();
-        System.out.println(t.solution(n, str));
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : t.solution(n, arr)) {
+            System.out.println(x + " ");
+        }
+
     }
 }
