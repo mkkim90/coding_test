0f502d3c07d80d5f12897a595df206a28b137e08
mkkim90 <mkzzang0928@gmail.com>
2022-04-03 18:16:02 +0900

    feat : 가위바위보

---
+++ src/main/java/inflearn/가위_바위_보.java
@@ -0,0 +1,34 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class 가위_바위_보 {
+    public String solution(int n, int[] a, int[] b) {
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            if (a[i] == b[i]) answer += "D";
+            else if (a[i] == 1 && b[i] == 3) answer += "A";
+            else if (a[i] == 2 && b[i] == 1) answer += "A";
+            else if (a[i] == 3 && b[i] == 2) answer += "A";
+            else answer += "B";
+        }
+        return answer;
+    } 
+
+    public static void main(String[] args) {
+        가위_바위_보 t = new 가위_바위_보();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = kb.nextInt();
+        }
+        System.out.println(t.solution(n, a, b));
+
+    }
+}
