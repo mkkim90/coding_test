692c1ff1bed52f205228d6613684f15bddb7077e
mkkim90 <mkzzang0928@gmail.com>
2022-04-04 12:10:35 +0900

    feat : 등수 구하기

---
+++ src/main/java/inflearn/등수_구하기.java
@@ -0,0 +1,28 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class 등수_구하기 {
+
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[i]) cnt++;
+            }
+            answer[i] = cnt;
+        }
+    }
+
+    public static void main(String[] args) {
+        등수_구하기 t = new 등수_구하기();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : t.solution(n, arr)) System.out.println(x + " ");
+    }
+}
