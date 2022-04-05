fdad081aa4888b8de0c0c834d754a1a960a071b0
mkkim90 <mkzzang0928@gmail.com>
2022-04-05 11:24:32 +0900

    feat : 두 배열 합치기

---
+++ src/main/java/inflearn/두_배열_합치기.java
@@ -0,0 +1,36 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class 두_배열_합치기 {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) answer.add(a[p1++]);
+            else answer.add(b[p2++]);
+        }
+        while (p1 < n) answer.add(a[p1++]);
+        while (p2 < m) answer.add(a[p2++]);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        두_배열_합치기 t = new 두_배열_합치기();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = kb.nextInt();
+        }
+        for (int x : t.solution(n, m, a, b)) {
+            System.out.println(x + " ");
+        }
+    }
+}
