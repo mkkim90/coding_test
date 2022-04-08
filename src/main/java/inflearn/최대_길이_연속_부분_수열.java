27198c812af466655987b8a2987f99235b2b0280
mkkim90 <mkzzang0928@gmail.com>
2022-04-08 22:08:35 +0900

    feat : 최대 길이 연속 부분 수열

---
+++ src/main/java/inflearn/최대_길이_연속_부분_수열.java
@@ -0,0 +1,30 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class 최대_길이_연속_부분_수열 {
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > k) {
+                if (arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        최대_길이_연속_부분_수열 t = new 최대_길이_연속_부분_수열();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(t.solution(n, m, arr));
+    }
+}
