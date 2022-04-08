3a4c3ad03f5c9083952d224e2bb23f30e7753473
mkkim90 <mkzzang0928@gmail.com>
2022-04-08 23:06:27 +0900

    feat : 아나그램 해쉬

---
+++ src/main/java/inflearn/아나그램_해쉬.java
@@ -0,0 +1,27 @@
+package inflearn;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class 아나그램_해쉬 {
+    public String solution(String s1, String s2) {
+        String answer = "YES";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : s1.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        for (char x : s2.toCharArray()) {
+            if (!map.containsKey(x) || map.get(x) == 0) return "NO";
+            map.put(x, map.get(x) -1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        아나그램_해쉬 t = new 아나그램_해쉬();
+        Scanner kb = new Scanner(System.in);
+        String a = kb.next();
+        String b = kb.next();
+        System.out.println(t.solution(a, b));
+    }
+}
